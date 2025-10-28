466759a849f8f0a5069e3ee1e1ca80a071614d73
Mohammed Naser <mnaser@vexxhost.com>
2025-10-28 10:55:17 -0400

    Initial commit
    Signed-off-by: Mohammed Naser <mnaser@vexxhost.com>

---
+++ flake.nix
@@ -0,0 +1,57 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+    treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
+
+    devshell.url = "github:numtide/devshell";
+    devshell.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.treefmt-nix.flakeModule
+        inputs.devshell.flakeModule
+      ];
+
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      perSystem =
+        {
+          config,
+          self',
+          inputs',
+          pkgs,
+          system,
+          ...
+        }:
+        {
+          treefmt = {
+            programs.nixfmt = {
+              enable = true;
+            };
+          };
+
+          devshells.default = {
+            packages =
+              with pkgs;
+              [
+                python311
+                uv
+              ]
+              ++ (builtins.attrValues config.treefmt.build.programs);
+          };
+        };
+
+      flake = {
+      };
+    };
+}
